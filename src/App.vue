ecf9b2bdd11df261dd4671d904155f4aba771e3e
Xavier Campos <xcampos@brooklynsteamcenter.org>
2023-05-18 13:52:37 -0400

    worked on filter

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import {ref, watch} from 'vue'
 let todos = ref(JSON.parse(window.localStorage.getItem('todos')) ?? [])
 let NewTodos = ref('')
 let input = ref ('')
+let filter = ref('all')
 
 watch(todos, function(value) {
   window.localStorage.setItem('todos',JSON.stringify(value))
@@ -24,6 +25,18 @@ function AddTodo () {
 function DeleteTodos (index) {
     todos.value.splice(index, 1)
   }
+
+
+function todoFilter (todo) {
+  if (filter.value == 'active') {
+    return todo.complete == false
+  } else if (filter.value == 'complete') {
+    return todo.complete == true
+  } else {
+    return true
+  }
+  
+}
 </script>
 
 
@@ -31,8 +44,20 @@ function DeleteTodos (index) {
   <body>
 <h1>My Todo Application</h1>
 
+<p v-if="todos.length > 0">
+ <input name="filter" type="radio" value="all" v-model="filter">
+ <label>All</label>
+
+ <input name="filter" type="radio" value="active" v-model="filter">
+ <label>Active</label>
+
+ <input name="filter" type="radio" value="complete" v-model="filter">
+ <label>Complete</label>
+</p>
+
+
 <ul>
-<li v-for="(todo, index) in todos" :class= "{completed: todo.complete}">
+<li v-for="(todo, index) in todos.filter(todoFilter)" :class= "{completed: todo.complete}">
   <input class="checkbox" type="checkbox" v-model="todo.complete" >
   <button @click="DeleteTodos(index)">🚮</button>
   {{ todo.text }}
@@ -44,6 +69,9 @@ function DeleteTodos (index) {
 
 </body>
 </template>
+
+
+
 
 
 <style>
